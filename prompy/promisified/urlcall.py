8dd5179f915ac9accde51cfdaefde7cf25b185b9
T4rk1n <t4rk@outlook.com>
2018-03-11 22:28:12 -0400

    Add json_call, get, post to urlcall

--- prompy/promisified/urlcall.py
+++ prompy/promisified/urlcall.py
@@ -1,7 +1,8 @@
-import enum
-from typing import NamedTuple
-from urllib import request, parse, robotparser, error
+import json
+from typing import NamedTuple, Dict, Any
+from urllib import request, parse, error
 
+from prompy.errors import UrlCallError
 from prompy.promise import Promise
 
 HTTP_GET = 'GET'
@@ -19,19 +20,22 @@ class UrlCallResponse(NamedTuple):
     reason: str
 
 
-class UrlCallError(Exception):
-    """Web call error"""
+def encode_url_params(url: str, params: Dict[str, Any]) -> str:
+    data = parse.urlencode(params)
+    return f"{url}?{data}"
 
 
-def url_call(url, prom_type=Promise, **kwargs) -> Promise:
+def url_call(url, data=None, headers=None, origin_req_host=None, unverifiable=False, method=None,
+             prom_type=Promise, **kwargs) -> Promise:
     def starter(resolve, reject):
         try:
-            req = request.Request(url)
+            req = request.Request(url, data=data, headers=headers or {},
+                                  origin_req_host=origin_req_host, method=method, unverifiable=unverifiable)
             with request.urlopen(req) as rep:
                 content_type = rep.headers.get_content_type()
-                headers = {}
+                rep_headers = {}
                 for k, v in rep.headers.items():
-                    headers[k] = v
+                    rep_headers[k] = v
                 content = rep.read()
                 resolve(UrlCallResponse(url, content_type, content, rep.status, headers, rep.msg, rep.reason))
         except error.HTTPError as e:
@@ -40,11 +44,34 @@ def url_call(url, prom_type=Promise, **kwargs) -> Promise:
     return prom_type(starter, **kwargs)
 
 
-def post(url, data=None):
-    pass
+def post(url, data=None, prom_type=Promise, **kwargs) -> Promise:
+    return url_call(url, method=HTTP_POST, data=data, prom_type=prom_type, **kwargs)
 
 
-if __name__ == '__main__':
-    from prompy.threaded.tpromise import TPromise
-    p = url_call("http://www.google.com", prom_type=TPromise)
-    p.then(lambda r: print(r))
+def get(url, params: dict=None, prom_type=Promise, **kwargs) -> Promise:
+    if params:
+        url = encode_url_params(url, params)
+    return url_call(url, prom_type=prom_type, **kwargs)
+
+
+def put(url, data, prom_type=Promise, **kwargs) -> Promise:
+    return url_call(url, data, prom_type=prom_type, **kwargs)
+
+
+def json_call(url, payload=None, encoding='UTF-8', prom_type=Promise, headers=None, **kwargs) -> Promise:
+    headers = headers or {}
+
+    def starter(resolve, reject):
+        pay = json.dumps(payload) if payload else None
+        headers['Content-Type'] = f'application/json ; charset={encoding}'
+        call = url_call(url, data=pay.encode(encoding), prom_type=prom_type, headers=headers, **kwargs)
+
+        def on_call_end(rep: UrlCallResponse):
+            if 'application/json' in rep.content_type:
+                resolve(json.loads(rep.content))
+            else:
+                reject(UrlCallError(f"json_call: Content-Type is not json: {rep.content_type}"))
+
+        call.then(on_call_end).catch(reject)
+    return prom_type(starter)
+
